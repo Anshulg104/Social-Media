e8e9a139d3d06c65f65bee185dc22045bf82f86d
Tuhin810 <tuhin.thakur1233@gmail.com>
2024-05-14 00:49:40 +0530

    BUILD:1

--- src/components/main/profile/profilePost/Profilepost.tsx
+++ src/components/main/profile/profilePost/Profilepost.tsx
@@ -9,6 +9,7 @@ const images = [
   { id: 3, uri: 'https://images.pexels.com/photos/20068318/pexels-photo-20068318/free-photo-of-a-man-in-a-brown-jacket-standing-in-the-middle-of-a-street.jpeg?auto=compress&cs=tinysrgb&w=600&lazy=load' },
   { id: 4, uri: 'https://images.pexels.com/photos/23279639/pexels-photo-23279639/free-photo-of-a-solar-eclipse-is-seen-in-the-sky.jpeg?auto=compress&cs=tinysrgb&w=600&lazy=load' },
   { id: 5, uri: 'https://images.pexels.com/photos/20555579/pexels-photo-20555579/free-photo-of-castillo-de-valdecorneja.jpeg?auto=compress&cs=tinysrgb&w=600&lazy=load' },
+  { id: 6, uri: 'https://images.pexels.com/photos/20555579/pexels-photo-20555579/free-photo-of-castillo-de-valdecorneja.jpeg?auto=compress&cs=tinysrgb&w=600&lazy=load' },
   
 ];
 
@@ -16,7 +17,7 @@ const Profilepost = () => {
   return (
     <ScrollView contentContainerStyle={styles.container}>
       {images.map((image, index) => (
-        <TouchableOpacity key={image.id} style={[styles.imageContainer, index === 0 ? styles.firstImage : null]}>
+        <TouchableOpacity key={image.id} style={[styles.imageContainer, index === 0 ? styles.firstImage : index === 4? styles.fourthImage : index===5? styles.fifthImage :null]}>
           <Image source={{ uri: image.uri }} style={styles.image} />
         </TouchableOpacity>
       ))}
@@ -28,21 +29,29 @@ const styles = StyleSheet.create({
   container: {
     flexDirection: 'row',
     flexWrap: 'wrap',
+    marginBottom:70
   },
   imageContainer: {
     width: windowWidth / 3.2, // Assuming 3 images per row
     height: windowWidth / 3, // Square images
-    padding: 6, // Adjust spacing as needed
+    padding: 1, // Adjust spacing as needed
   },
   firstImage: {
     width: (windowWidth / 3.2) * 2, 
+    height: (windowWidth / 3) * 2, 
+  },
+  fourthImage: {
+ marginTop: - (windowWidth / 3) 
+  },
+  fifthImage:{
+marginLeft: (windowWidth / 3.2) * 2,
+marginTop: - (windowWidth / 3) 
   },
   image: {
     flex: 1,
     width: '100%',
     height: '100%',
     resizeMode: 'cover',
-    borderRadius: 5,
   },
 });
 
